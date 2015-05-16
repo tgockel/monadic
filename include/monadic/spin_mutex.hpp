555e4705e178075876965675ae3e3b3fad3bf33e
Travis Gockel <travis@gockelhut.com>
2015-05-15 21:06:32 -0600

    Adds documentation for spin_mutex.

--- include/monadic/spin_mutex.hpp
+++ include/monadic/spin_mutex.hpp
@@ -18,6 +18,9 @@
 namespace monadic
 {
 
+/** A mutex type which spins on an atomic bool instead of relying on OS functions. When your work unit takes less time
+ *  than your OS's quantum and your lock has low contention, a spin mutex can be faster than a regular mutex.
+**/
 class spin_mutex
 {
 public:
@@ -29,8 +32,15 @@ public:
     { }
     
     spin_mutex(const spin_mutex&) = delete;
+    spin_mutex(spin_mutex&&)      = delete;
     spin_mutex& operator=(const spin_mutex&) = delete;
+    spin_mutex& operator=(spin_mutex&&)      = delete;
     
+    /** Attempt to lock this mutex. When the lock is acquired, there is a sequentially-consistent guarantee. If the lock
+     *  is not acquired, there is no memory ordering guarantee.
+     *  
+     *  \returns \c true if the lock was obtained; \c false if it was not (someone else holds the lock).
+    **/
     bool try_lock()
     {
         bool hopeful_val = false;
@@ -42,32 +52,67 @@ public:
                                                            );
     }
     
+    /** Attempt to acquire the lock on this mutex until the specified \a expiry_time. If the given time is in the past,
+     *  this function will still attempt to acquire the lock once (this prevents the lock from becoming unobtainable on
+     *  slow machines with short spin lengths).
+     *  
+     *  \param expiry_time The absolute time to give up spinning at. You most likely want to use a monotonic clock
+     *    (such as \c std::chrono::steady_clock) for this so you are not subject to NTP or other clock changes.
+     *  
+     *  \returns \c true if the lock was obtained; \c false if it was not (someone else holds the lock).
+    **/
     template <typename TClock, typename TDuration>
     bool try_lock_until(const std::chrono::time_point<TClock, TDuration>& expiry_time)
     {
-        while (TClock::now() < expiry_time)
+        do
+        {
             if (try_lock())
                 return true;
+        } while (TClock::now() < expiry_time);
         return false;
     }
     
+    /** Attempt to acquire the lock on this mutex for the specified \a duration.
+     *  
+     *  \param duration The relative time to spin before giving up. The actual ticking time is based on
+     *    \c std::chrono::steady_clock.
+     *  
+     *  \returns \c true if the lock was obtained; \c false if it was not (someone else holds the lock).
+    **/
     template <typename TRep, typename TPeriod>
     bool try_lock_for(const std::chrono::duration<TRep, TPeriod>& duration)
     {
         return try_lock_until(std::chrono::steady_clock::now() + duration);
     }
     
+    /** Attempt to acquire the lock for the specified number of \a spins.
+     *  
+     *  \param spins The number of times to call \c try_lock before giving up.
+     *  
+     *  \returns \c true if the lock was obtained; \c false if it was not (someone else holds the lock).
+    **/
+    bool try_lock_spins(std::size_t spins)
+    {
+        while (spins --> 0)
+            if (try_lock())
+                return true;
+        return false;
+    }
+    
+    /** Repeatedly attempt to acquire a lock on this mutex in a while loop. **/
     void lock()
     {
         while (!try_lock())
             continue;
     }
     
+    /** Unlock this mutex. There is no checking that you actually own the mutex. **/
     void unlock()
     {
         locked.store(false, std::memory_order_seq_cst);
     }
     
+    /** Get a pointer to the atomic variable that backs this mutex. Please do not edit it. **/
     native_handle_type native_handle()
     {
         return &locked;
